72395a92edf26c2aa7cc43bf7f41022334c84cd4
Regina <AbdurakhmanRA@mpei.ru>
2021-09-05 19:03:20 +0300

    code: modified

--- Source.cpp
+++ Source.cpp
@@ -7,31 +7,32 @@
 
 using namespace std;
 
-#define MAX_KEY_LENGTH 255
 #define INFO_BUF_SIZE 32767
 #define BUFSIZE MAX_PATH
 #define FILESYSNAMEBUFSIZE MAX_PATH
 #define TOTALBYTES    8192
 
-//CPU performance measurement
-double freq = 0.0;
-__int64 tick = 0;
+//2.1 CPU performance measurement
+__int64 freq, tick1, tick2 = 0;
 
 void counter()
 {
     LARGE_INTEGER CNT;
+    QueryPerformanceCounter(&CNT);
+    tick1 = CNT.QuadPart;
     QueryPerformanceFrequency(&CNT);
     freq = double(CNT.QuadPart);
-    printf("CPU performance frequency: %u Hz \n", CNT);
-    QueryPerformanceCounter(&CNT);
-    tick = CNT.QuadPart;
+    printf("Рабочая частота ЦП: %u Hz \n", CNT);
+
 }
 
+//2.2 время выполнения пункта 2.1
 double tickCount()
 {
     LARGE_INTEGER CNT;
     QueryPerformanceCounter(&CNT);
-    double count = double((CNT.QuadPart - tick) * 1000000 / freq) ;
+    tick2 = CNT.QuadPart;
+    double count = double((tick2 - tick1) * 1000000 / freq);
     return count;
 }
 
@@ -79,8 +80,8 @@ int main()
         
         if (GetDiskFreeSpaceEx(buffer, &available, &total, &free) != 0)
         {
-            printf("Total size: %u  bytes\n", total.QuadPart);
-            printf("Free space: %u  bytes\n", free.QuadPart);
+            printf("Total size: %I64u  bytes\n", total.QuadPart);
+            printf("Free space: %I64u  bytes\n", free.QuadPart);
         }
         else
         {
@@ -97,39 +98,57 @@ int main()
     //5)Startup programs
 
     HKEY hKey;
-    TCHAR ValueName[MAX_KEY_LENGTH];
+    TCHAR ValueName[TOTALBYTES];
     DWORD chValueName;
-    DWORD retCode;
-
+    DWORD retCode, QueryErr;
+    LPBYTE  lpData = (LPBYTE)malloc(TOTALBYTES);
     DWORD BufferSize = TOTALBYTES;
-    PPERF_DATA_BLOCK PerfData = (PPERF_DATA_BLOCK)malloc(BufferSize);
-    DWORD cbData = BufferSize;
+    DWORD cbData = TOTALBYTES;
     int i = 0;
 
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
     {
         printf("\nStartup programs:\n");
+        while (TRUE)
+        {
+            chValueName = TOTALBYTES;
+            retCode = RegEnumValue(hKey, i, ValueName, &chValueName, NULL, NULL, NULL, NULL);
+            if (retCode == ERROR_SUCCESS)
+            {
+                QueryErr = RegQueryValueEx(hKey, ValueName, NULL, NULL, lpData, &cbData);
+                if (QueryErr == ERROR_SUCCESS)
+                {
+                    printf("%d) %s:  %s\n", i + 1, ValueName, lpData);
+                    i++;
+                }
+                else
+                {
+                    int code = GetLastError();
+                    printf("\nFunction  failed! Error %d.\n\n", code);
+                }
+            }
+            else
+            {
+                if (retCode == ERROR_NO_MORE_ITEMS)
+                {
+                    printf("No more items available.\n\n");
+                }
+                break;
+            }
+
+        }
     }
     else printf("\nFunction RegOpenKeyEx() failed!\n");
 
-    while (TRUE) {
-        chValueName = sizeof(ValueName);
-        retCode = RegEnumValue(hKey, i, ValueName, &chValueName, NULL, NULL, NULL, NULL);
-        if (retCode == ERROR_SUCCESS)
-        {
-            RegQueryValueEx(hKey, ValueName, NULL, NULL, (LPBYTE)PerfData, &cbData);
-            printf("%d) %s:  %s\n", (i + 1), ValueName, PerfData);
-            i++;
-        }
-        else break;
-    }
 
     RegCloseKey(hKey);
 
 
     //CPU performance measurement
+
     counter();
-    cout << "CPU clock count: " << tickCount() << " us\n";
+    double t = tickCount();
+    printf("Количество тактов ЦП потраченных на выполнение пункта 2.1: %f мкс\n", t);
     return 0;
 
 }
