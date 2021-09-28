1bd71ebf4d663a3098069291b175ca1039edfbc5
Regina <AbdurakhmanRA@mpei.ru>
2021-09-28 22:18:57 +0300

    update

--- Source.cpp
+++ Source.cpp
@@ -10,30 +10,21 @@ using namespace std;
 #define INFO_BUF_SIZE 32767
 #define BUFSIZE MAX_PATH
 #define FILESYSNAMEBUFSIZE MAX_PATH
-#define TOTALBYTES    8192
+#define TOTALBYTES 8192
 
-//2.1 CPU performance measurement
-__int64 freq, tick1, tick2 = 0;
+//2.1, 2.2
+
 
 void counter()
 {
-    LARGE_INTEGER CNT;
-    QueryPerformanceCounter(&CNT);
-    tick1 = CNT.QuadPart;
-    QueryPerformanceFrequency(&CNT);
-    freq = double(CNT.QuadPart);
-    printf("Рабочая частота ЦП: %u Hz \n", CNT);
+    LARGE_INTEGER freq, tick1, tick2;
+    QueryPerformanceCounter(&tick1);
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&tick2);
+    double count = (double(tick2.QuadPart - tick1.QuadPart) * 1000000 / freq.QuadPart);
+    printf("Рабочая частота ЦП: %u Hz \n", freq);
+    printf("Количество тактов ЦП потраченных на выполнение пункта 2.1: %f мкс\n", count);
 
-}
-
-//2.2 время выполнения пункта 2.1
-double tickCount()
-{
-    LARGE_INTEGER CNT;
-    QueryPerformanceCounter(&CNT);
-    tick2 = CNT.QuadPart;
-    double count = double((tick2 - tick1) * 1000000 / freq);
-    return count;
 }
 
 int main()
@@ -99,57 +90,24 @@ int main()
 
     HKEY hKey;
     TCHAR ValueName[TOTALBYTES];
-    DWORD chValueName;
-    DWORD retCode, QueryErr;
+    DWORD chValueName = TOTALBYTES;
     LPBYTE  lpData = (LPBYTE)malloc(TOTALBYTES);
-    DWORD BufferSize = TOTALBYTES;
-    DWORD cbData = TOTALBYTES;
     int i = 0;
 
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+    printf("\nStartup programs:\n");
+    RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_READ, &hKey);
+    while (RegEnumValue(hKey, i, ValueName, &chValueName, NULL, NULL, NULL, NULL) == ERROR_SUCCESS)
     {
-        printf("\nStartup programs:\n");
-        while (TRUE)
-        {
-            chValueName = TOTALBYTES;
-            retCode = RegEnumValue(hKey, i, ValueName, &chValueName, NULL, NULL, NULL, NULL);
-            if (retCode == ERROR_SUCCESS)
-            {
-                QueryErr = RegQueryValueEx(hKey, ValueName, NULL, NULL, lpData, &cbData);
-                if (QueryErr == ERROR_SUCCESS)
-                {
-                    printf("%d) %s:  %s\n", i + 1, ValueName, lpData);
-                    i++;
-                }
-                else
-                {
-                    int code = GetLastError();
-                    printf("\nFunction  failed! Error %d.\n\n", code);
-                }
-            }
-            else
-            {
-                if (retCode == ERROR_NO_MORE_ITEMS)
-                {
-                    printf("No more items available.\n\n");
-                }
-                break;
-            }
-
-        }
+        chValueName = TOTALBYTES;
+        RegQueryValueEx(hKey, ValueName, NULL, NULL, lpData, &chValueName);
+        printf("%d) %s:  %s\n", i + 1, ValueName, lpData);
+        i++;
     }
-    else printf("\nFunction RegOpenKeyEx() failed!\n");
-
-
     RegCloseKey(hKey);
-
 
     //CPU performance measurement
 
     counter();
-    double t = tickCount();
-    printf("Количество тактов ЦП потраченных на выполнение пункта 2.1: %f мкс\n", t);
     return 0;
-
 }
 
